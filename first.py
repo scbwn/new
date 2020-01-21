aa457d7ba903cd1863bfa138f4baebb11347fa20
scbwn <59886320+scbwn@users.noreply.github.com>
2020-01-21 18:44:45 -0500

    First version of first code

---
+++ first.py
@@ -0,0 +1,5 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello world!")
+Hello world!
+>>> 
